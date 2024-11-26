43a65bcccb7ebfcda5bdec821b12fde97a80c07c
K0ut <jkoutsou@student.fitchburgstate.edu>
2024-11-26 10:30:20 -0500

    github
    start of the github

---
+++ Assets/Scripts/DetectCollision.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+    private int objectsDestroyed = 0; // Counter for destroyed objects
+
+    // Update is called once per frame
+    void Update()
+    {
+        // You can handle other game logic here if needed
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject); // Destroy this object
+        Destroy(other.gameObject); // Destroy the other object
+
+        objectsDestroyed++; // Increment the counter
+        CheckGameOver(); // Check for game over condition
+    }
+
+    private void CheckGameOver()
+    {
+        if (objectsDestroyed >= 6)
+        {
+            // Trigger game over logic here
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        // Implement game over logic, such as displaying a UI message or stopping the game
+        Debug.Log("Game Over! You destroyed 6 objects.");
+
+    }
+}
